e07d88f13e1499d588271e66c2fd4310a9455acd
Austin <austinrf2918@yahoo.com>
2017-03-02 02:24:35 -0500

    Added has-wordpress-api.

--- src/clo_wp/core.clj
+++ src/clo_wp/core.clj
@@ -1,4 +1,23 @@
-(ns clo-wp.core)
+(ns clo-wp.core
+  (:require [clj-http.client :as client]
+             [cheshire.core :refer :all]))
+
+(defrecord WordPressConnection [url username password])
+
+(defn has-wordpress-api
+  [url]
+  (let [response (client/head "http://site.com/resource" {:accept :json})]
+    if
+
+(defn connect
+  [url username password]
+  (if (has-wordpress-api url)
+    (->WordPressConnection url username password)
+    (throw (IllegalArgumentException. "This URL does not have a cooresponding WordPress API."))))
+
+(defn get-posts
+  [wordpress-connection]
+  )
 
 (defn foo
   "I don't do a whole lot."
