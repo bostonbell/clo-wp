242583906a2edb51dfb7bef80e5c61b29323f71d
Austin <austinrf2918@yahoo.com>
2017-03-04 20:47:36 -0500

    Added documentation to core.

--- src/clo_wp/core.clj
+++ src/clo_wp/core.clj
@@ -3,10 +3,12 @@
             [cheshire.core :refer :all]))
 
 (defn build-api-endpoint
+  "Function used generally for internal use to build endpoint paths."
   ([url ep] (str url "/wp-json/wp/v2" ep))
   ([url] (str url "/wp-json/wp/v2")))
 
 (defn has-wordpress-api
+  "Takes a url and makes sure that the WP Rest API is on a website."
   [url]
   (print (build-api-endpoint url))
   (try
@@ -17,14 +19,35 @@
 (defrecord WordPressConnection [url username password])
 
 (defn connect
+  "Attempts to connect to a site which has WordPress installed.
+
+  Takes a url which must be properly formatted (see below), a username, and a password.
+  Returns a WordPressConnection instance that we are pretty sure can be used in all the
+  libraries functions.
+
+  Note that the URL must be fully formatted: http:// or https:// must be included, otherwise
+  an error will occur.
+
+  In certain cases where the JSON api can not be found, an IllegalArgumentException will be 
+  thrown, and likewise for not including an HTTP header."
   [url username password]
-  (if (clojure.string/includes? url "http://")
+  (if (or (clojure.string/includes? url "http://") (clojure.string/includes? url "https://")
     (if (has-wordpress-api url)
         (->WordPressConnection url username password)
-        (throw (IllegalArgumentException. "This URL does not have a cooresponding WordPress API.")))
+        (throw (IllegalArgumentException. "This URL does not seem to have a cooresponding WordPress API or does not exist.")))
     (throw (IllegalArgumentException. "URL did not include http header: Try using the fully qualified URL."))))
 
 (defn get-from-wordpress
+  "Raw API call for receiving data from a site that has WordPress installed.
+  
+  Takes an instantiated WordPressConnection, the path of an endpoint (formatted with an initial /), and
+  the context one wishes to access the endpoint with. Legitimate contexts include :view :edit and :embed.
+  Certain contexts allow access to additional information in the response, for example allowing gets 
+  on posts to recieve the raw markup (including shortcodes) when the edit parameter is passed instead of
+  view, which will only return 'rendered' markup.
+
+  Primary for internal use or for extending the library."
+
   ([wordpress-connection endpoint-path context]
    (client/get
     (build-api-endpoint (:url wordpress-connection) (str endpoint-path "?context=" (name context)))
@@ -38,6 +61,16 @@
     (get-from-wordpress wordpress-connection endpoint-path :edit)))
 
 (defn delete-from-wordpress
+  "Raw API call for deleting data from a site that has WordPress installed.
+
+  Takes an instantiated WordPressConnection, the path of an endpoint (formatted with an initial /), and
+  the context one wishes to access the endpoint with. Legitimate contexts include :view :edit and :embed.
+  Certain contexts allow access to additional information in the response, for example allowing gets 
+  on posts to recieve the raw markup (including shortcodes) when the edit parameter is passed instead of
+  view, which will only return 'rendered' markup.
+
+  Primary for internal use or for extending the library."
+
   ([wordpress-connection endpoint-path context]
    (client/delete
     (build-api-endpoint (:url wordpress-connection) (str endpoint-path "?context=" (name context)))
@@ -51,6 +84,16 @@
     (delete-from-wordpress wordpress-connection endpoint-path :edit)))
 
 (defn post-to-wordpress
+  "Raw API call for posting data from a site that has WordPress installed.
+
+  Takes an instantiated WordPressConnection, the path of an endpoint (formatted with an initial /), and
+  the context one wishes to access the endpoint with. Legitimate contexts include :view :edit and :embed.
+  Certain contexts allow access to additional information in the response, for example allowing gets 
+  on posts to recieve the raw markup (including shortcodes) when the edit parameter is passed instead of
+  view, which will only return 'rendered' markup.
+
+  Primary for internal use or for extending the library."
+
   ([wordpress-connection endpoint-path context data]
    (client/post
     (build-api-endpoint (:url wordpress-connection) (str endpoint-path "?context=" (name context)))
@@ -65,6 +108,14 @@
     (post-to-wordpress wordpress-connection endpoint-path :edit data)))
 
 (defn get-site-information
+  "Gets raw information about a site that has WordPress installed.
+
+  Takes an instantiated WordPressConnection and returns the informations from
+  the /wp-json endpoint for us.
+
+  Generally not the most useful thing in the world unless checking for extensions
+  on the site itself."
+
   [wordpress-connection]
   (:body (client/get (str (:url wordpress-connection) "/wp-json")
                      {:basic-auth [(:username wordpress-connection)
